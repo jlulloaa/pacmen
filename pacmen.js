5994544f535bd4e307a3962047b52cbd1b44a9a3
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-05-18 00:04:33 -0400

    Added extra control buttons Stop and Reset
    Added additional buttons to control de operation of the pacmen. However, still cannot solve the area issue to move the pacman

--- pacmen.js
+++ pacmen.js
@@ -4,7 +4,19 @@ const pacArray = [
   ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
 
-const pacMen = []; // This array holds all the pacmen
+var pacMen = []; // This array holds all the pacmen
+// This variable controls the execution of the actions
+var timer = null;
+// Control the buttons
+// At the start of the game, only create pacman is enable:
+var create_button = document.getElementById("create"); 
+var start_button = document.getElementById("start");
+var stop_button = document.getElementById("stop");
+var reset_button = document.getElementById("reset");
+
+start_button.disabled=true;
+stop_button.disabled=true;
+reset_button.disabled=true;
 
 // This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
 var focus = 0;
@@ -13,22 +25,36 @@ var focus = 0;
 shrink_size = 50;
 normal_size = 100;
 
+// Default size of the container holding the pacmen
+var area = {x: 100, y:100};
+
 // This function returns an object with random values
-function setToRandom(scale) {
+function setToRandom(scalex, scaley=0) {
+  if (scaley==0){
+    scaley=scalex;
+  }
   return {
-    x: Math.random() * scale,
-    y: Math.random() * scale,
+    x: Math.random() * scalex,
+    y: Math.random() * scaley,
   };
 }
 
 // Factory to make a PacMan at a random position with random velocity
 function makePac() {
+  // As soon as we create one pacman, the start game becomes active and have to ensure the reset holds inactive:
+  start_button.disabled=false;
+
+  // Define the area id=game
+  let game = document.getElementById('game');
+  // Update the area size:
+  // area.x = parseInt(game.style.width, 10);
+  // area.y = parseInt(game.style.height, 10);
+  console.log('Area: ' + game.style.width);
   // returns an object with random values scaled {x: 33, y: 21}
   let velocity = setToRandom(10); // {x:?, y:?}
-  let position = setToRandom(200);
-
+  let position = setToRandom(area.x, area.y);
+    
   // Add image to div id = game
-  let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
 
@@ -47,7 +73,7 @@ function makePac() {
   // TODO: set position here
   newimg.style.top = position.y;
   newimg.style.left = position.x;
-
+  console.log('Pacman position (x,y): ('+position.x + ',' + position.y + ')')
   // Modifies the colour of the pacman randomly:
   // newimg.style.webkit.filter = 'hue-rotate(180deg)';
   let hueRotLevel = Math.random() * 360;
@@ -69,6 +95,11 @@ function makePac() {
 }
 
 function update() {
+  // As soon as we press start, the start button becomes disabled, and the stop becomes active:
+  start_button.disabled=true;
+  stop_button.disabled=false;
+  reset_button.disabled=true;
+
   // loop over pacmen array and move each one and move image in DOM
   pacMen.forEach((item) => {
     item.newimg.width = normal_size;
@@ -83,7 +114,7 @@ function update() {
 
     item.newimg.src = pacArray[item.directionX][item.randomIndex]    
   });
-  setTimeout(update, 50);
+  timer = setTimeout(update, 20);
 }
 
 function checkCollisions(item) {
@@ -102,6 +133,25 @@ function checkCollisions(item) {
         item.newimg.height=shrink_size;        
         item.newimg.width=normal_size+0.5*shrink_size;
       }
+}
+
+function reset() {
+  // Reset the pacmen array to empty
+  location.reload();
+  pacMen = [];
+  // Once we press reset, the button create become active and everything else, inactive: 
+  create_button.disabled=false;
+  start_button.disabled=true;
+  stop_button.disabled=true;
+  reset_button.disabled=true;
+}
+
+function stop(item) {
+  clearTimeout(timer);
+  // Once we press stop, the buttons start and reset become active, while stop becomes inactive
+  start_button.disabled=false;
+  stop_button.disabled=true;
+  reset_button.disabled=false;
 }
 
 function makeOne() {
